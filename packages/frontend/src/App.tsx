e99ae8fe65b46488dbeda71dde038dc3c211a1f6
Doug Clinton <doug@wellfactored.com>
2024-01-09 22:29:10 +0100

    Re-work things a bit

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {LoginPage} from "./LoginPage";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider, useNavigate} from "react-router-dom";
 
 const router = createBrowserRouter([
     {path: "/", element: <LoginPage/>}, // Change this to be the home page when it is ready
@@ -9,6 +9,7 @@ const router = createBrowserRouter([
 ])
 
 function App() {
+    useNavigate()
     return (
         <RouterProvider router={router}/>
     );
--- packages/frontend/src/LoginPage.tsx
+++ packages/frontend/src/LoginPage.tsx
@@ -5,10 +5,7 @@ import axios from "axios";
 import {Col, Container, Row} from "react-bootstrap";
 
 export function LoginPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    function submit() {
+    function submit(email: string, password: string) {
         axios.post("http://localhost:3001/api/login", {email: email, password: password})
     }
 
@@ -17,29 +14,41 @@ export function LoginPage() {
             <Row>
                 <Col/>
                 <Col>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"
-                                          value={email}
-                                          onChange={(e) => setEmail(e.target.value)}/>
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"
-                                          onChange={(e) => setPassword(e.target.value)}/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit" onClick={submit}>
-                            Log in
-                        </Button>
-                    </Form>
+                    <LoginForm onSubmit={submit}/>
                 </Col>
                 <Col/>
             </Row>
         </Container>
+    )
+}
+
+type LoginFormProps = {
+    onSubmit: (email: string, password: string) => void
+}
+
+const LoginForm = (props: LoginFormProps) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email"
+                              onChange={(e) => setEmail(e.target.value)}/>
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password"
+                              onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={() => props.onSubmit(email, password)}>
+                Log in
+            </Button>
+        </Form>
     )
 }
