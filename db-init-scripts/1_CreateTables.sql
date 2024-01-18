750080a030f8a7f3eee93459cc3a84571ae26e9a
sebastian clinton <sebastianclinton@icloud.com>
2024-01-18 23:42:18 +0100

    created tables in the database

--- db-init-scripts/1_CreateTables.sql
+++ db-init-scripts/1_CreateTables.sql
@@ -10,13 +10,38 @@ CREATE TABLE "user"
 CREATE TABLE competitor
 (
     id      BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES "user" (id),
+    nationality TEXT NOT NULL,
+    date_of_birth DATE NOT NULL,
+    emergency_contact TEXT 
+);
+
+CREATE TABLE organiser (
+    id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES "user" (id)
+);
+
+
+CREATE TABLE venue (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    contact_email TEXT NOT NULL
 );
 
 CREATE TABLE event
 (
-    id BIGSERIAL PRIMARY KEY
+    id BIGSERIAL PRIMARY KEY,
+    venue_id BIGINT NOT  NULL REFERENCES "venue" (id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    country TEXT NOT NULL,
+    stars INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    contact_email TEXT NOT NULL
 );
+
 
 CREATE TABLE event_entry
 (
@@ -25,3 +50,11 @@ CREATE TABLE event_entry
 );
 
 CREATE UNIQUE INDEX "event_entry_idx" ON event_entry (competitor_id, event_id);
+
+CREATE TABLE event_organiser (
+    event_id BIGINT NOT NULL REFERENCES event (id),
+    organiser_id BIGINT NOT NULL REFERENCES organiser (id)
+);
+
+CREATE UNIQUE INDEX "event_organiser_idx" ON event_organiser (organiser_id, event_id);
+
