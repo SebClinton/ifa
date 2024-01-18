1da545b7dc5d1979148c0973956c25a5098e8b56
Doug Clinton <doug@wellfactored.com>
2024-01-18 17:48:30 +0100

    Start fleshing out sql for creating tables

--- db-init-scripts/1_CreateTables.sql
+++ db-init-scripts/1_CreateTables.sql
@@ -1,8 +1,27 @@
-
-CREATE TABLE "user" (
-    id BIGSERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+CREATE TABLE "user"
+(
+    id          BIGSERIAL PRIMARY KEY,
+    email       TEXT NOT NULL UNIQUE,
+    password    TEXT NOT NULL,
     family_name TEXT NOT NULL,
     given_names TEXT NOT NULL
 );
+
+CREATE TABLE competitor
+(
+    id      BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES "user" (id)
+);
+
+CREATE TABLE event
+(
+    id BIGSERIAL PRIMARY KEY
+);
+
+CREATE TABLE event_entry
+(
+    competitor_id BIGINT NOT NULL REFERENCES competitor (id),
+    event_id      BIGINT NOT NULL REFERENCES event (id)
+);
+
+CREATE UNIQUE INDEX "event_entry_idx" ON event_entry (competitor_id, event_id);
