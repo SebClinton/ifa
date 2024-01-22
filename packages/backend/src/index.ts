42dc2912ec260b59d1c5cb8746369c057f91017f
Doug Clinton <doug@wellfactored.com>
2024-01-22 20:58:02 +0100

    Create a database connection pool in the server and make a sample query call

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import {Pool} from 'pg';
 
 dotenv.config();
 
@@ -18,13 +19,25 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000;
 
+const connectionPool = new Pool({
+    host: process.env.PG_HOST,
+    port: Number.parseInt(process.env.PG_PORT || "5432"),
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server for the win');
 });
 
 app.post('/api/login', (req, res) => {
     console.log(req.body);
-    res.send('call to login');
+    // example of how to query the db and send the results in the http response
+    connectionPool.query("select NOW()").then((result) => res.send(result.rows))
 })
 
 app.listen(port, () => {
